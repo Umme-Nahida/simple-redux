2ff16eb4292755507cdad86e08b918a65732fa19
Developer Nahida <ummenahida20@gmail.com>
2025-02-12 01:59:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Counter from './Components/Counter/Counter'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>This simple redux applications when I created this  </h1>
+      <div>
+        <Counter></Counter>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
