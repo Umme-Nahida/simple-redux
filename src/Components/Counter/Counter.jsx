fcab70d00532a977d4140a7e29b4e951e6d64ae4
Developer Nahida <ummenahida20@gmail.com>
2025-02-14 03:31:38 +0600

    added increment and decrement

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+    const [count,setCount] = useState(0)
+
+    const handleIncrement = ()=>{
+    setCount((preCount)=> preCount + 1)
+    }
+
+    const handleDecrement = ()=>{
+    setCount((preCount)=> preCount - 1)
+    }
+
     return (
         <div>
-            this is counters 
+            this is  
+            <h1>count {count} </h1>
+
+            <button onClick={handleIncrement}>Increment</button> 
+            <button onClick={handleDecrement}>Decrement</button> 
         </div>
     );
 };
