f19a31915513110c407945f7e9a03fbd70bbc9de
Developer Nahida <ummenahida20@gmail.com>
2025-03-23 23:03:48 +0600

    initial theme added

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Stats = () => {
+const Stats = ({totalCount}) => {
     return (
-        <div>
-            this is stats 
+        <div className='bg-gray-200 p-10 text-center'>
+           <h1>Total Count: {totalCount} </h1>
         </div>
     );
 };
